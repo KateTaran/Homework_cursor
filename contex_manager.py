8b2623eda76435630af5eb326cb086b7403d3a5e
Kate Taran <ktaran16@gmail.com>
2021-03-15 21:56:20 +0200

    done

--- contex_manager.py
+++ contex_manager.py
@@ -1,4 +1,7 @@
-#1. словник елементами якого буде пара ключ:значення де ключ - мітка часу, значення - репліка в даний момент часу
+import pickle
+import openpyxl
+
+#1.1 словник елементами якого буде пара ключ:значення де ключ - мітка часу, значення - репліка в даний момент часу
 dict_from_text = {}
 lst = []
 file = open("task1.txt", "r")
@@ -14,7 +17,7 @@ for el in lst:
         dict_from_text[key_1] = content
 print(dict_from_text)
 
-#2. файл в якому знаходиться текст з якого видалені всі мітки часу. всі субтитри повинні мати вигляд простого тексту.
+#1.2 файл в якому знаходиться текст з якого видалені всі мітки часу. всі субтитри повинні мати вигляд простого тексту.
 lst_1 = []
 file = open("task1.txt", "r")
 lst_1 = file.readlines()
@@ -32,5 +35,28 @@ print(my_string)
 #     if len(el) > 5:
 #         print (el)
 
+#2. в файлі task2 збережений список, відкрийте цей файл, прочитайте вміст, і знайдіть середнє арифметичне чисел що знаходяться в списку
+file = open("task2", "rb")
+lst_3 = pickle.load(file)
+print(lst_3)
+s_a = sum(lst_3)/len(lst_3)
+print(s_a)
+
+#3. Використовуючи openpyxl (або будь-яку іншу зручну для вас бібліотеку), напишіть контекстний менеджер для роботи з ексель.
+#Даний менеджер повинен бути аналогом методу open()
+class OpenEx:
+    def __init__(self, file_name):
+        self.file_obj = openpyxl.load_workbook(file_name)
+
+    def __enter__(self):
+        return self.file_obj
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.file_obj.close()
 
 
+with OpenEx('task3.xlsx') as file:
+    sheet = file.active
+    sheet['A1'] = 100
+    print(sheet['A1'].value)
+    file.save('task3.xlsx')
